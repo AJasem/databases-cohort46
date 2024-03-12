97ef67dba77ca743bd314f08a8655a0b7dd6e40d
AJasem <ajassem.ahmad@gmail.com>
2024-03-12 17:40:27 +0100

    Ahmad-week4-db

--- Week4/assaignment/ex2-transactions/index.js
+++ Week4/assaignment/ex2-transactions/index.js
@@ -3,5 +3,7 @@ import transfer from "./transfer.js";
 setup();
 setTimeout(() => {
   transfer(101, 102, 1000, "transfer");
+}, 1000);
+setTimeout(() => {
   transfer(101, 103, 500, "gift");
 }, 1000);
